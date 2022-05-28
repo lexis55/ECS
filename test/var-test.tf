b42ebe5e038f5206ee467d288eaa250073f1f6f8
virtuoholics <104276880+virtuoholics@users.noreply.github.com>
2022-05-28 21:44:58 +0500

    added files to test folder

---
+++ test/var-test.tf
@@ -0,0 +1,28 @@
+variable "cluster-launch-type" {
+  description = "Launch Type of ECS Cluster"
+  type        = string
+  default     = "FARGATE"
+}
+
+variable "test-port" {
+  description = "Port on which ECS container listens."
+  type        = number
+  default     = 80
+}
+
+variable "cpu_architecture" {
+  description = "CPU Architecture for the Task."
+  type        = string
+  default     = "X86_64"
+}
+
+variable "operating_system_family" {
+  description = "OS Family for the Task."
+  type        = string
+  default     = "LINUX"
+}
+
+variable "test-container-image" {
+  type    = string
+  default = "nginxdemos/hello"
+}
