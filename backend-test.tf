daab10fe33a7855fcca5f09c90eeb1a8cde1e659
virtuoholics <104276880+virtuoholics@users.noreply.github.com>
2022-05-30 17:54:04 +0500

    Delete backend-test.tf

--- backend-test.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ansong844-terraform-backend-bucket"
-    key            = "terraform-backend/test/terraform.tfstate"
-    region         = var.region
-    dynamodb_table = "backend-state-lock"
-    encrypt        = true
-  }
-}
