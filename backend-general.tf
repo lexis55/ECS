7cd0ee8d0440e5e6a54bf2f2a9afb81dd485c706
virtuoholics <104276880+virtuoholics@users.noreply.github.com>
2022-05-30 17:44:30 +0500

    uploaded files to new branch general

---
+++ backend-general.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "ansong844-terraform-backend-bucket"
+    key            = "terraform-backend/general/terraform.tfstate"
+    region         = var.region
+    dynamodb_table = "ansong844-backend-state-lock"
+    encrypt        = true
+  }
+}
